9e7aff7c8ab6486fcc88738ee55ff62582f3bffe
Konstantinos-Semertzidis1 <konstantinos.semertzidis1@ibm.com>
2020-12-23 16:08:36 +0000

    Fair Walk

--- src/node2vec.py
+++ src/node2vec.py
@@ -1,19 +1,59 @@
 import numpy as np
-import networkx as nx
 import random
 
 
-class Graph():
-	def __init__(self, nx_G, is_directed, p, q):
+class Graph:
+	def __init__(self, nx_G, is_directed, p, q, groups):
 		self.G = nx_G
 		self.is_directed = is_directed
 		self.p = p
 		self.q = q
+		self.groups = groups
+
+	def node2vec_fair_walk(self, walk_length, start_node):
+		"""
+		Simulate a random fair walk starting from start node.
+		"""
+		G = self.G
+		walk = [start_node]
+
+		while len(walk) < walk_length:
+			cur = walk[-1]
+			cur_nbrs = sorted(G.neighbors(cur))
+			if len(cur_nbrs) > 0:
+				group = random.randint(0, len(self.groups))
+				group_cur_nbrs = [nbr for nbr in cur_nbrs if G.nodes[nbr]["group"] == group]
+				if len(group_cur_nbrs) > 0:
+					next_ = random.choice(group_cur_nbrs)
+				else:
+					next_ = random.choice(cur_nbrs)
+				prev = cur if len(walk) == 1 else walk[-2]
+				walk.append(next_)
+			else:
+				break
+
+		return walk
+
+	def simulate_fair_walks(self, num_walks, walk_length):
+		"""
+		Repeatedly simulate random walks from each node.
+		"""
+		G = self.G
+		walks = []
+		nodes = list(G.nodes())
+		print('Walk iteration:')
+		for walk_iter in range(num_walks):
+			print(str(walk_iter+1), '/', str(num_walks))
+			random.shuffle(nodes)
+			for node in nodes:
+				walks.append(self.node2vec_fair_walk(walk_length=walk_length, start_node=node))
+
+		return walks
 
 	def node2vec_walk(self, walk_length, start_node):
-		'''
+		"""
 		Simulate a random walk starting from start node.
-		'''
+		"""
 		G = self.G
 		alias_nodes = self.alias_nodes
 		alias_edges = self.alias_edges
@@ -28,24 +68,23 @@ class Graph():
 					walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
 				else:
 					prev = walk[-2]
-					next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], 
-						alias_edges[(prev, cur)][1])]
-					walk.append(next)
+					next_ = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], alias_edges[(prev, cur)][1])]
+					walk.append(next_)
 			else:
 				break
 
 		return walk
 
 	def simulate_walks(self, num_walks, walk_length):
-		'''
+		"""
 		Repeatedly simulate random walks from each node.
-		'''
+		"""
 		G = self.G
 		walks = []
 		nodes = list(G.nodes())
-		print 'Walk iteration:'
+		print('Walk iteration:')
 		for walk_iter in range(num_walks):
-			print str(walk_iter+1), '/', str(num_walks)
+			print(str(walk_iter+1), '/', str(num_walks))
 			random.shuffle(nodes)
 			for node in nodes:
 				walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
@@ -53,14 +92,15 @@ class Graph():
 		return walks
 
 	def get_alias_edge(self, src, dst):
-		'''
+		"""
 		Get the alias edge setup lists for a given edge.
-		'''
+		"""
 		G = self.G
 		p = self.p
 		q = self.q
 
 		unnormalized_probs = []
+		com_prob = 1.0
 		for dst_nbr in sorted(G.neighbors(dst)):
 			if dst_nbr == src:
 				unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
@@ -69,14 +109,14 @@ class Graph():
 			else:
 				unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
 		norm_const = sum(unnormalized_probs)
-		normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+		normalized_probs = [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 
 		return alias_setup(normalized_probs)
 
 	def preprocess_transition_probs(self):
-		'''
+		"""
 		Preprocessing of transition probabilities for guiding the random walks.
-		'''
+		"""
 		G = self.G
 		is_directed = self.is_directed
 
@@ -84,11 +124,10 @@ class Graph():
 		for node in G.nodes():
 			unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
 			norm_const = sum(unnormalized_probs)
-			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+			normalized_probs = [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 			alias_nodes[node] = alias_setup(normalized_probs)
 
 		alias_edges = {}
-		triads = {}
 
 		if is_directed:
 			for edge in G.edges():
@@ -105,11 +144,11 @@ class Graph():
 
 
 def alias_setup(probs):
-	'''
+	"""
 	Compute utility lists for non-uniform sampling from discrete distributions.
 	Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
 	for details
-	'''
+	"""
 	K = len(probs)
 	q = np.zeros(K)
 	J = np.zeros(K, dtype=np.int)
@@ -117,33 +156,34 @@ def alias_setup(probs):
 	smaller = []
 	larger = []
 	for kk, prob in enumerate(probs):
-	    q[kk] = K*prob
-	    if q[kk] < 1.0:
-	        smaller.append(kk)
-	    else:
-	        larger.append(kk)
+		q[kk] = K*prob
+		if q[kk] < 1.0:
+			smaller.append(kk)
+		else:
+			larger.append(kk)
 
 	while len(smaller) > 0 and len(larger) > 0:
-	    small = smaller.pop()
-	    large = larger.pop()
+		small = smaller.pop()
+		large = larger.pop()
 
-	    J[small] = large
-	    q[large] = q[large] + q[small] - 1.0
-	    if q[large] < 1.0:
-	        smaller.append(large)
-	    else:
-	        larger.append(large)
+		J[small] = large
+		q[large] = q[large] + q[small] - 1.0
+		if q[large] < 1.0:
+			smaller.append(large)
+		else:
+			larger.append(large)
 
 	return J, q
 
+
 def alias_draw(J, q):
-	'''
+	"""
 	Draw sample from a non-uniform discrete distribution using alias sampling.
-	'''
+	"""
 	K = len(J)
 
 	kk = int(np.floor(np.random.rand()*K))
 	if np.random.rand() < q[kk]:
-	    return kk
+		return kk
 	else:
-	    return J[kk]
+		return J[kk]
